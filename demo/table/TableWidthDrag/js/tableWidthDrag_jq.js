b74a773f780a2419e5841b7074e354b530b6891d
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-10-16 09:12:25 +0800

    2019年10月16日 08:12:08

--- demo/table/TableWidthDrag/js/tableWidthDrag_jq.js
+++ demo/table/TableWidthDrag/js/tableWidthDrag_jq.js
@@ -1,32 +1,42 @@
 // Created by wxc on 2019/10/12
 
 // 初始化
-let dragState = false;
+let dragState = false, onDrag = false;
+let dragTarget = '';
 
 function TableDragEvent () {
     // 表头鼠标移动事件
-    $(document).find('table th').on('mousemove', function (element) {
-        let bodyTrNumber = $(document).find('table tbody > tr');
+    $(document).find('table th').on('mousemove', function (e) {
+        let bodyTrNumber = $(document).find('table tbody > tr').length;
+        let target = $(e.currentTarget);
 
-        if (bodyTrNumber === 0) {
+        if (bodyTrNumber === 0)
             return;
-        }
 
-        if (dragState) {
+        if (onDrag) {
 
-        } else if (IsOnBorder(element)) {
+        } else if (IsOnBorder(e, true)) {
+            dragTarget = target.prev();
+
+            if (dragTarget.length === 0)
+                return;
+
             dragState = true;
-            $(element.currentTarget).css("cursor", "col-resize");
+            target.css("cursor", "col-resize");
+        } else if (IsOnBorder(e, false)) {
+            dragTarget = target;
+            dragState = true;
+            target.css("cursor", "col-resize");
         } else {
             dragState = false;
-            $(element.currentTarget).css("cursor", "default");
+            target.css("cursor", "default");
         }
     })
 }
 
 // 判断指针当前是否在边框附近
-function IsOnBorder (element) {
-
+function IsOnBorder (e, left) {
+    return left ? e.pageX <= ($(e.target).offset().left + 2) : e.pageX >= ($(e.target).offset().left + $(e.target).outerWidth() - 2);
 }
 
 $(function () {
